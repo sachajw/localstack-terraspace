830821da64d7541ddbaa2dc466142173065d8cbc
Sacha Wharton <32266273+sachajw@users.noreply.github.com>
2023-10-10 22:08:19 +0200

    🐛 FIX: DEVOPS-63 ruby config

--- infra/config/app.rb
+++ infra/config/app.rb
@@ -1,23 +1,18 @@
 require 'aws-sdk-s3'
 
 Aws.config.update(
-  endpoint: 'http://s3.local.gd:4566',
-  credentials: Aws::SharedCredentials.new(profile_name: 'localstack-dev', access_key_id: 'fake', secret_access_key: 'fake', ignore_configured_endpoint_urls: 'false'),
+  endpoint: 'https://localhost.localstack.cloud:4566',
+  credentials: Aws::SharedCredentials.new(profile_name: 'localstack-dev', access_key_id: 'fake', secret_access_key: 'fake'),
+#  credentials: Aws::Credentials.new(profile_name: 'localstack-dev', access_key_id: 'fake', secret_access_key: 'fake'),
+#  credentials: Aws::SharedCredentials.new,
   s3: {
+    endpoint: 'https://s3.localhost.localstack.cloud:4566',
+#    access_key_id: 'fake',
+#    secret_access_key: 'fake',
     http_wire_trace: true,
-    force_path_style: true,
-    validate_params: false, # Skip credential validation
-    ignore_configured_endpoint_urls: false,
-    endpoint_discovery: true,
-    profile: "localstack-dev",
-    access_key_id: "fake",
-    secret_access_key: "fake"
-
+    profile: 'localstack-dev',
+    force_path_style: true
   }
-  #skip_requesting_account_id: true,
-  #skip_metadata_api_check: true,
-  #s3: {force_path_style: 'true'},
-  #s3: {http_wire_trace: 'true'},
 )
 
 # Docs: https://terraspace.cloud/docs/config/reference/
@@ -28,5 +23,5 @@ Terraspace.configure do |config|
   # copy_modules setting introduced 2.2.5 to speed up terraspace build
   # See: https://terraspace.cloud/docs/config/reference
   config.build.copy_modules = true
-  config.build.cache_dir	= ":REGION/:APP/:ROLE/:ENV/:BUILD_DIR" # without :REGION
+  config.build.cache_dir	= ":REGION/:APP/:ROLE/:ENV/:BUILD_DIR"
 end
--- infra/config/terraform/provider.tf
+++ infra/config/terraform/provider.tf
@@ -5,13 +5,3 @@
 # provider "aws" {
 #   region = "us-east-1"
 # }
-
-provider "aws" {
-  region                      = "eu-west-1"
-  access_key                  = "fake"
-  secret_key                  = "fake"
-  skip_credentials_validation = true
-  skip_metadata_api_check     = true
-  skip_requesting_account_id  = true
-  s3_force_path_style         = true
-}
