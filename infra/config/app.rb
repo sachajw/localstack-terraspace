8bf5d38b95d65d23bc65dddbba30b9a992c83f73
Sacha Wharton <32266273+sachajw@users.noreply.github.com>
2023-10-10 23:17:06 +0200

    🐛 FIX: DEVOPS-63 error validating provider credentials

--- infra/config/app.rb
+++ infra/config/app.rb
@@ -3,9 +3,9 @@ require 'aws-sdk-s3'
 Aws.config.update(
   endpoint: 'https://localhost.localstack.cloud:4566',
   #credentials: Aws::SharedCredentials.new(profile_name: 'localstack-dev', access_key_id: 'fake', secret_access_key: 'fake'),
-  access_key_id: 'fake',
-  secret_access_key: 'fake',
-  session_token: 'fake',
+  access_key_id: 'AKIAIOSFODNN7EXAMPLE',
+  secret_access_key: 'wJalrXUtnFEMI/K7MDENG/bPxRfiCYzEXAMPLEKEY',
+  session_token: 'AQoEXAMPLEH4aoAH0gNCAPyJxz4BlCFFxWNE1OPTgk5TthT+FvwqnKwRcOIfrRh3c/LTo6UDdyJwOOvEVPvLXCrrrUtdnniCEXAMPLE/IvU1dYUg2RVAJBanLiHb4IgRmpRV3zrkuWJOgQs8IZZaIv2BXIa2R4OlgkBN9bkUDNCJiBeb/AXlzBBko7b15fjrBs2+cTQtpZ3CYWFXG8C5zqx37wnOE49mRl/+OtkIKGO7fAE',
 #  credentials: Aws::Credentials.new(profile_name: 'localstack-dev', access_key_id: 'fake', secret_access_key: 'fake'),
 #  credentials: Aws::SharedCredentials.new,
   s3: {
@@ -26,5 +26,5 @@ Terraspace.configure do |config|
   # copy_modules setting introduced 2.2.5 to speed up terraspace build
   # See: https://terraspace.cloud/docs/config/reference
   config.build.copy_modules = true
-  config.build.cache_dir = ":REGION/:APP/:ROLE/:ENV/:BUILD_DIR"
+  config.build.cache_dir =  ":REGION/:APP/:ROLE/:ENV/:BUILD_DIR"
 end
